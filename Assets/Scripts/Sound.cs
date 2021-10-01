958b3ff2a87106508911da6c5a260f4b33dcbfa5
unknown <watercan101@gmail.com>
2021-10-01 17:19:33 -0400

    Animation commit

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -7,10 +7,11 @@ public class Sound : MonoBehaviour
     public AudioSource musicSource;
     public AudioClip musicClipOne;
     public AudioClip musicClipTwo;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,19 +21,23 @@ public class Sound : MonoBehaviour
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
+            anim.SetInteger("State",1);
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
+            anim.SetInteger("State",0);
         }
          if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State",2);
         }
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
+            anim.SetInteger("State",0);
         }
          if (Input.GetKeyDown(KeyCode.L))
         {
@@ -43,5 +48,9 @@ public class Sound : MonoBehaviour
         {
           musicSource.loop = false;
         }
+        if (Input.GetKey("escape"))
+{
+Application.Quit();
+}
     }
 }
